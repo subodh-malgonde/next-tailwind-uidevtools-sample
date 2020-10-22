0c2a66fb1ceca3a5bb75f2d5a681a3be68f3e44d
Subodh Malgonde <subodh.malgonde@gmail.com>
2020-10-22 10:40:20 +0530

    Setup ui-devtools, installed sass, added tailwind config

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import '@ui-devtools/tailwind'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+    defaultLineHeights: true,
+    standardFontWeights: true
+  },
+  purge: ['./styles/tailwind.scss','./src/components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          '100': '#F7F9FC',
+          '200': '#E6E9F0',
+          '300': '#C5CEE0',
+          '400': '#B3BCD1',
+          '500': '#9BACCA',
+          '600': '#6679A3',
+          '700': '#334C85',
+          '800': '#001F66',
+          '900': '#00133D',
+        },
+        red: {
+          '100': '#FBEEED',
+          '200': '#F9E3E3',
+          '300': '#EDA9A7',
+          '400': '#E8918E',
+          '500': '#E27571',
+          '600': '#DB524E',
+          '700': '#C54A46',
+          '800': '#993937',
+          '900': '#6E2927',
+        },
+        green: {
+          '600': '#27AE60',
+        },
+        gradientColorStops: {
+          'starting': '#001F66',
+          'ending': '#1F49AD'
+        },
+        hover: "#1F49AD",
+        link: "#18A0FB"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
